087d598b6098ba1c9cafde41fdbf32532538ab36
Karen Vu <karenkv@uci.edu>
2019-03-30 12:34:59 -0700

    Twilio text function and route home page

--- app.py
+++ app.py
@@ -3,5 +3,9 @@ import flask
 
 app = Flask(__name__)
 
+@app.route("/",methods=["GET"])
+def main():
+    return render_template("index.html")
+
 if __name__ == '__main__':
     app.run()
--- twilio_helper.py
+++ twilio_helper.py
@@ -8,3 +8,7 @@ class TwilioHelper():
         self.account_sid = creds["twilio"]["account_sid"]
         self.auth_token = creds["twilio"]["auth_token"]
         self.client = Client(self.account_sid, self.auth_token)
+        self.twilio_number = creds["twilio"]["number"]
+
+    def text_user(self, message: str, user_number: str) -> None:
+        message = self.client.messages.create(body = message, to = user_number, from_ = self.twilio_number)
