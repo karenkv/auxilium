b25bb49ac9bf5d404ef21a2d8eb2332c4ba10b26
Jason Le <36774043+Jasonleh99@users.noreply.github.com>
2019-03-30 17:13:49 -0700

    Added types to organizations

--- JSONFormHandler.py
+++ JSONFormHandler.py
@@ -1,40 +1,46 @@
 import json
 
-databaseDictionary = "orgDatabase.json"
-sampleInput = "sampleInput.json"
+class JSONFormHandler:
+    def __init__(self):
+        self.databaseDictionary = "orgDatabase.json"
+        self.sampleInput = "sampleInput.json"
 
-def getKey(orgDict):
-    return orgDict["name"] + " " + orgDict["location"]
+    def getKeyFromDictionary(self, orgDict):
+        return orgDict["name"] + " " + orgDict["location"]
 
-def addOrg(form):
-    jDict = getDictFromJSON(form)
-    database = getDictFromJSON(databaseDictionary)
-    try:
-        temp = database[getKey(jDict)]
-        return False
-    except:
-        database[getKey(jDict)] = jDict
-        with open('orgDatabase.json', 'w') as fp:
-            json.dump(database, fp)
-        return True
+    def getKeyFromJSON(self, path):
+        dictionary = self.getDictFromJSON(path)
+        return self.getKeyFromDictionary(dictionary)
 
-def getOrg(key):
-    database = getDictFromJSON(databaseDictionary)
-    try:
-        return database[getKey(jDict)]
-    except:
-        return "Org not found!"
+    def addOrg(self, form):
+        jDict = self.getDictFromJSON(form)
+        database = self.getDictFromJSON(self.databaseDictionary)
+        try:
+            temp = database[self.getKeyFromDictionary(jDict)]
+            return False
+        except:
+            database[self.getKeyFromDictionary(jDict)] = jDict
+            with open('orgDatabase.json', 'w') as fp:
+                json.dump(database, fp)
+            return True
 
-def removeOrg(key):
-    database = getDictFromJSON(databaseDictionary)
-    try:
-        org = database[key]
-        del database[key]
-        with open('orgDatabase.json', 'w') as fp:
-            json.dump(database, fp)
-        return org
-    except:
-        return None
+    def getOrg(self, key):
+        database = self.getDictFromJSON(self.databaseDictionary)
+        try:
+            return database[key]
+        except:
+            return "Org not found!"
 
-def getDictFromJSON(path):
-    return json.load(open(path))
+    def removeOrg(self, key):
+        database = self.getDictFromJSON(self.databaseDictionary)
+        try:
+            org = database[key]
+            del database[key]
+            with open('orgDatabase.json', 'w') as fp:
+                json.dump(database, fp)
+                return org
+        except:
+            return None
+
+    def getDictFromJSON(self, path):
+        return json.load(open(path))
