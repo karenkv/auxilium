209d28626ae8f75a62b6ecdccd4270554570525a
immmjimmy <immmjimmy@gmail.com>
2019-03-30 22:35:38 -0700

    wip organizations post request

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/",methods=["GET"])
+@app.route("/", methods=["GET"])
 def main():
     return render_template("index.html")
 
-@app.route("/organizations",methods=["GET"])
+@app.route("/organizations", methods=["GET"])
 def organizations():
     return render_template("organizations.html")
+
+@app.route("/add-new-org", methods=["POST"])
+def add_new_org():
+    req_data = request.form
+
+    # print(req_data)
+
+    # with open('org_database.json') as f:
+    #     data = json.load(f)
+    
+    # data.update(req_data)
+
+    # with open('org_database.json', 'w') as f:
+    #     json.dump(data, f)
+
+    return 'done'
 
 if __name__ == '__main__':
     app.run()
---
+++ static/js/organizations.js
@@ -0,0 +1,48 @@
+$(document).ready(() => {
+
+    $('#submit-btn').click(() => {
+        const orgName = $('#org-name').val().trim();
+        const foodChecked = $('#food-check').prop('checked');
+        const hygieneChecked = $('#hygiene-check').prop('checked');
+        const shelterChecked = $('#shelter-check').prop('checked');
+        const address = $('#address').val().trim();
+        const phoneNumber = $('#phone-number').val().trim();
+        const website = $('#website').val().trim();
+
+        const typesArray = []
+        if(foodChecked == true) {
+            typesArray.push('food');
+        }
+
+        if(hygieneChecked == true) {
+            typesArray.push('hygiene');
+        }
+
+        if(shelterChecked == true) {
+            typesArray.push('shelter');
+        }
+
+        const newOrg = {
+            [orgName]: {
+                "types": typesArray,
+                "website": website,
+                "phone": phoneNumber,
+                "name": orgName,
+                "location": address
+            }
+        };
+
+        console.log(newOrg);
+
+        // i literally don't know what to do
+        
+        // $.post('/add-new-org', {
+        //         "types": typesArray,
+        //         "website": website,
+        //         "phone": phoneNumber,
+        //         "name": orgName,
+        //         "location": address
+        // });
+    });
+
+});
