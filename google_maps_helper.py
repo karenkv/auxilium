ecc25fb6a64db5ac1dae6ab1c94f0c1220815869
Karen Vu <karenkv@uci.edu>
2019-03-30 17:04:25 -0700

    Edited helpers and added docstrings

--- google_maps_helper.py
+++ google_maps_helper.py
@@ -5,17 +5,25 @@ from json import load
 
 class GoogleMapsHelper():
     def __init__(self):
+        '''
+        Initializes GoogleMapsHelper class with api_key from credentials json file and sets url for distance matrix api.
+        '''
         with open("credentials.json") as f:
             creds = load(f)
         self.api_key = creds["google"]["api_key"]
         self.url_distance = "https://maps.googleapis.com/maps/api/distancematrix"
-        self.url_direction = "https://maps.googleapis.com/maps/api/directions"
 
     def build_url_dist(self, dest: str, origin: str) -> str:
-        query_param = [('units', 'imperial'), ('origins', origin), ('destinations', dest), ('key', self.api_key)]
+        '''
+        Builds the URL for the DistanceMatrix API by taking in a destination and origin. Returns a URL string.
+        '''
+        query_param = [('units', 'imperial'), ('origins', origin), ('destinations', dest), ('key', self.api_key)] # Unit set to imperial
         return self.url_distance + '/json?' + urllib.parse.urlencode(query_param)
 
     def get_dist(self, url: str) -> dict:
+        '''
+        Handles URL request response and returns a dictionary of the json response if accepted.
+        '''
         response = None
         try:
             response = urllib.request.urlopen(url)
@@ -25,6 +33,9 @@ class GoogleMapsHelper():
                 response.close()
 
     def get_min(self, locations: list) -> list:
+        '''
+        Given a list of locations, returns a list of sorted distances from closest to farthest by using the DistanceMatrix API.
+        '''
         latlong = self.getLocation()
         origin = str(latlong[0]) + ", " + str(latlong[1])
         distances = dict()
@@ -33,7 +44,19 @@ class GoogleMapsHelper():
             dist = self.get_dist(url)
             distances[location] = float(dist["rows"][0]["elements"][0]["distance"]["text"].strip(" mi").replace(",", ""))
         sorted_distances = sorted(distances.items(), key=lambda kv: kv[1])
-        return sorted_distances
+        if(sorted_distances is None):
+            return None
+        i = 0
+        min = []
+        for item in sorted_distances:
+            min.append(item)
+            i += 1
+            if i == 3:
+                break
+        return min
 
     def getLocation(self):
+        '''
+        Finds latitude and longitude given user's IP address.
+        '''
         return geocoder.ip('me').latlng
--- twilio_helper.py
+++ twilio_helper.py
@@ -5,6 +5,9 @@ import os
 
 class TwilioHelper():
     def __init__(self):
+        '''
+        Initializes TwilioHelper class with account_sid, auth_token, client, and phone number with credentials json file.
+        '''
         with open("credentials.json") as f:
             creds = load(f)
         self.account_sid = creds["twilio"]["account_sid"]
@@ -13,6 +16,10 @@ class TwilioHelper():
         self.twilio_number = creds["twilio"]["number"]
 
     def message_response(self, incoming_msg) -> (str, str):
+        '''
+        Handles messaging response from client. Takes an incoming message and returns tuple with user's number and service requested,
+        unless incoming message is hello/bye/invalid.
+        '''
         resp = MessagingResponse()
         incoming_msg.toLower()
         if(incoming_msg == "Hello"):
@@ -26,12 +33,21 @@ class TwilioHelper():
                 resp.message("Please follow this link to confirm location-based services: \n")
             elif(incoming_msg == "Personal Hygiene"):
                 resp.message("Please follow this link to confirm location-based services: \n")
+            else:
+                resp.message("Invalid response. Please try again.")
+                return ("", "")
             user_number = get_user_number()
             return (user_number, incoming_msg)
         return ("", "")
 
-    def get_user_number(self):
+    def get_user_number(self) -> str:
+        '''
+        Gets user number from client messages list.
+        '''
         return str(client.messages.list(to = self.twilio_number)[0].from_)
 
     def text_user(self, message: str, user_number: str) -> None:
+        '''
+        Takes a message and phone number and texts the user's number the specified message.
+        '''
         message = self.client.messages.create(body = message, to = user_number, from_ = self.twilio_number)
